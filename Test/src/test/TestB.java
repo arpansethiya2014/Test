4785204566111656593f3bd7f68fd872ae01530f
arpansethiya78699 <arpansethiya2014@gmail.com>
2020-09-22 22:56:58 +0530

    Updated TestB.java

--- Test/src/test/TestB.java
+++ Test/src/test/TestB.java
@@ -4,7 +4,8 @@ public class TestB {
      public static void main(String[] args) {
 		for (int i = 0; i <=10; i++) {
 			System.out.println(i);
-			System.out.println("hello");
+			
 		}
+		System.out.println("hello");
 	}
 }
